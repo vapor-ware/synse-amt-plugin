43818239aca332a3e64559a55d4457299162c94f
Klemente <klemente@vapor.io>
2018-05-02 16:20:37 -0400

    initial commit for plugin framework

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# synse-amt-plugin
+# Synse AMT Plugin
+A general-purpose AMT plugin for [Synse Server][synse-server].
+
+
+> **NOTE**: This plugin is currently in development and is not ready for
+> use in any context. 
+
+
+[synse-server]: https://github.com/vapor-ware/synse-server
---
+++ devices/device.go
@@ -0,0 +1,23 @@
+package devices
+
+import (
+    "github.com/vapor-ware/synse-sdk/sdk"
+)
+
+// AmtPower is the handler for the AMT controller.
+var AmtPower = sdk.DeviceHandler{
+    Type:  "power",
+    Model: "AMT_POWER",
+
+    Read:  amtPowerRead,
+    Write: nil,
+}
+
+// amtPowerRead is the read handler function for amt power devices.
+func amtPowerRead(device *sdk.Device) ([]*sdk.Reading, error) {
+
+    readings := []*sdk.Reading{
+        sdk.NewReading("state", "on"),
+    }
+    return readings, nil
+}
